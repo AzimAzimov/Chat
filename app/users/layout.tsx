cf70bd6cbcab9e58dbf0a4b23a64761f13688e5a
Azim <azimov.an@veza.ru>
2023-11-16 09:20:58 +0300

    users

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,18 @@
 import React, { ReactNode } from "react";
 import Sidebar from "@/app/components/Sidebar/Sidebar";
+import getUsers from "@/app/actions/getUsers";
+import UserList from "@/app/users/components/UserList/UserList";
 
 interface UserLayoutProps {
   children: ReactNode;
 }
 
 const UserLayout = async ({ children }: UserLayoutProps) => {
+  const users = await getUsers();
+
   return (
     <Sidebar>
+      <UserList items={users} />
       <div className={"h-full"}>{children}</div>
     </Sidebar>
   );
